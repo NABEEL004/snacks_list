a9acaf229976b963839ae791bb759d669f338870
NABEEL004 <66759134+NABEEL004@users.noreply.github.com>
2024-05-30 11:45:13 +0800

    added styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Cross from "/cross.png"
 import './App.css'
 
 function App() {
@@ -6,7 +7,6 @@ function App() {
   const [newSnack, setNewSnack] = useState("")
 
   const PANTRY_API = import.meta.env.VITE_PANTRY_API
-
 
   const getData = async () => {
     try {
@@ -24,8 +24,13 @@ function App() {
 
   const handleSubmit = async (event) => {
     event.preventDefault()
-    setSnacks([...snacks, newSnack])
+    if (newSnack === "") {
+      return
+    }
+    const snackToAdd = newSnack
+    setSnacks([...snacks, snackToAdd])
     console.log(snacks)
+    setNewSnack("")
 
     try {
       const response = await fetch("https://getpantry.cloud/apiv1/pantry/" + PANTRY_API + "/basket/newBasket99", {
@@ -33,12 +38,11 @@ function App() {
         headers: {
           'Content-Type' : 'application/json',
         },
-        body: JSON.stringify({snacks: [...snacks, newSnack]})
+        body: JSON.stringify({snacks: [...snacks, snackToAdd]})
       })
       if (response.ok) {
         console.log("post request sent!")
       }
-      setNewSnack("")
     }
     catch (error) {
       console.error(error)
@@ -68,20 +72,38 @@ function App() {
   }
 
   return (
-    <>
-      <h1>
+    <div className='bg-orange-300 h-screen w-screen flex flex-col items-center justify-center gap-4'>
+      <h1 className='text-3xl font-bold font-mochiy text-slate-800'>
         Aliyah's Snacks
       </h1>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="name" id="name" value={newSnack} onChange={(e) => setNewSnack(e.target.value)}/>
-          <button>Add Snack!</button>
+      <div className='bg-white rounded-xl shadow-lg py-8 px-4 flex flex-col gap-4 w-[21rem]'>
+        <form onSubmit={handleSubmit} className='flex gap-4'>
+          <input 
+            type="text" 
+            name="name" 
+            value={newSnack} 
+            onChange={(e) => setNewSnack(e.target.value)}
+            className='border-slate-400 border-2 rounded-lg py-1 px-2'
+          />
+          <button 
+            className='bg-orange-600 rounded-lg py-1 px-2 cursor-pointer active:scale-95 active:bg-orange-700 font-mono text-white'
+          >
+            Add Snack!
+          </button>
         </form>
-        <ul>
-          {snacks.map(snack => <div><li>{snack}</li><span onClick={() => deleteSnack(snack)}>X</span></div>)}
+        <ul className='flex flex-col gap-1'>
+          {snacks.map(snack => (
+            <div className='group flex flex-row justify-between items-center px-8'>
+              <li className='font-mono list-disc text-slate-800'>{snack}</li>
+              <img 
+                src={Cross}
+                onClick={() => deleteSnack(snack)}
+                className='hidden group-hover:block cursor-pointer h-4' />
+            </div>
+            ))}
         </ul>
       </div>
-    </>
+    </div>
   )
 }
 
